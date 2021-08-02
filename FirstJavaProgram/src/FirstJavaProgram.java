d72fd729a23b4f8ed8d678c703ef8e9a844a9e86
ThanhNgo888 <thanhngo26@hotmail.com>
2021-08-02 13:04:04 -0700

    First Java Program

---
+++ FirstJavaProgram/src/FirstJavaProgram.java
@@ -0,0 +1,13 @@
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Coding Dojo");
+		System.out.println("I am 100 years old");
+		System.out.println("My hometown is Seattle, WA");
+	}
+
+}
+
+
